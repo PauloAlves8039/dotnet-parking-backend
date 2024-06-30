b582c27c6f385be8efa33982796eddef4f4bfadb
PauloAlves8039 <pj38alves@gmail.com>
2024-06-30 17:51:26 -0300

    feat: added mappings

---
+++ src/Parking.Service/Mappings/ParkingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Parking.Model.Models;
+using Parking.Service.DTOs;
+
+namespace Parking.Service.Mappings;
+
+public class ParkingProfile : Profile
+{
+    public ParkingProfile()
+    {
+        CreateMap<Customer, CustomerDTO>().ReverseMap();
+        CreateMap<Address, AddressDTO>().ReverseMap();
+        CreateMap<Vehicle, VehicleDTO>().ReverseMap();
+        CreateMap<CustomerVehicle, CustomerVehicleDTO>().ReverseMap();
+        CreateMap<Stay, StayDTO>().ReverseMap();
+    }
+}
