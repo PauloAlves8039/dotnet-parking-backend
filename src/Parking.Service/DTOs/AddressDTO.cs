caf2e371ecc345f30e9ac523fb851d1fe4e4b2e0
PauloAlves8039 <pj38alves@gmail.com>
2024-06-30 17:48:56 -0300

    feat: added dtos classes

---
+++ src/Parking.Service/DTOs/AddressDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Parking.Service.DTOs;
+
+public class AddressDTO
+{
+    public int Id { get; set; }
+
+    public string Street { get; set; }
+
+    public string Number { get; set; }
+
+    public string? Complement { get; set; }
+
+    public string Neighborhood { get; set; }
+
+    public string FederativeUnit { get; set; }
+
+    public string City { get; set; }
+
+    public string ZipCode { get; set; }
+}
