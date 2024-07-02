a952459a848aa60a58b847a66737657679d7ec94
PauloAlves8039 <pj38alves@gmail.com>
2024-07-01 21:50:37 -0300

    feat: created helper service

---
+++ src/Parking.Service/Helpers/Interfaces/IUtilities.cs
@@ -0,0 +1,13 @@
+﻿using Parking.Model.Interfaces;
+using Parking.Model.Models;
+using Parking.Service.DTOs;
+
+namespace Parking.Service.Helpers.Interfaces
+{
+    public interface IUtilities
+    {
+        Task UpdateStayStatus(int id, string newStatus, IRepository<Stay> repository);
+        double CalculateStayHours(StayDTO stayDTO);
+        decimal CalculateTotalAmount(double hours, decimal hourlyRate);
+    }
+}
---
+++ src/Parking.Service/Helpers/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using Parking.Model.Interfaces;
+using Parking.Model.Models;
+using Parking.Service.DTOs;
+using Parking.Service.Helpers.Interfaces;
+
+namespace Parking.Service.Helpers
+{
+    public class Utilities : IUtilities
+    {
+        public async Task UpdateStayStatus(int id, string newStatus, IRepository<Stay> repository)
+        {
+            var existingStay = await repository.GetByIdAsync(id);
+
+            if (existingStay != null)
+            {
+                existingStay.StayStatus = newStatus;
+                await repository.UpdateAsync(existingStay);
+            }
+        }
+
+  
+        public double CalculateStayHours(StayDTO stayDTO)
+        {
+            if (stayDTO.EntryDate == null || stayDTO.ExitDate == null)
+            {
+                return 0;
+            }
+
+            TimeSpan hoursDifference = stayDTO.ExitDate.Value - stayDTO.EntryDate.Value;
+            return hoursDifference.TotalHours;
+        }
+
+        public decimal CalculateTotalAmount(double hours, decimal hourlyRate)
+        {
+            decimal totalAmount = (decimal)hours * hourlyRate;
+            return totalAmount;
+        }
+    }
+}
