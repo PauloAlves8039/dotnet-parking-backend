6fe8ae9a467674d147c543b28829458ac5c9d5c4
PauloAlves8039 <pj38alves@gmail.com>
2024-07-02 20:11:27 -0300

    feat: added dependency injections

--- src/Parking.WebAPI/Program.cs
+++ src/Parking.WebAPI/Program.cs
@@ -1,6 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Parking.Data.Context;
+using Parking.Data.Repositories;
+using Parking.Model.Interfaces;
+using Parking.Service.Helpers.Interfaces;
+using Parking.Service.Helpers;
+using Parking.Service.Interfaces.Business;
+using Parking.Service.Interfaces;
+using Parking.Service.Mappings;
+using Parking.Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
+builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+builder.Services.AddScoped<IStayRepository, StayRepository>();
+builder.Services.AddScoped<ICustomerVehicleRepository, CustomerVehicleRepository>();
+
+builder.Services.AddScoped(typeof(IService<,>), typeof(Service<,>));
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IStayService, StayService>();
+builder.Services.AddScoped<ICustomerVehicleService, CustomerVehicleService>();
+
+builder.Services.AddScoped<IUtilities, Utilities>();
+builder.Services.AddAutoMapper(typeof(ParkingProfile));
 
 builder.Services.AddControllers();
 
