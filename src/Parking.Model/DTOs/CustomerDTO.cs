cc04e0a1375380fcd7254b987b965d683854e31d
PauloAlves8039 <pj38alves@gmail.com>
2024-07-06 10:42:44 -0300

    fix: implemented dtos in the model layer

---
+++ src/Parking.Model/DTOs/CustomerDTO.cs
@@ -0,0 +1,24 @@
+﻿using Parking.Model.Models;
+using System.Text.Json.Serialization;
+
+namespace Parking.Model.DTOs;
+
+public class CustomerDTO
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public string Cpf { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Email { get; set; }
+
+    public int? AddressId { get; set; }
+
+    [JsonIgnore]
+    public virtual Address Address { get; set; }
+}
---
+++ src/Parking.Model/DTOs/CustomerVehicleDTO.cs
@@ -0,0 +1,22 @@
+﻿using Parking.Model.Models;
+using System.Text.Json.Serialization;
+
+namespace Parking.Model.DTOs;
+
+public class CustomerVehicleDTO
+{
+    public int Id { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public int? VehicleId { get; set; }
+
+    [JsonIgnore]
+    public virtual Customer Customer { get; set; }
+
+    [JsonIgnore]
+    public virtual Vehicle Vehicle { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Stay> Stays { get; set; }
+}
